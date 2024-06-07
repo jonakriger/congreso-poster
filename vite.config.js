2b6bb38ab7d7a2f109b2046881beb5c7368705f6
Jona Kriger <jonathankriger@gmail.com>
2024-06-06 22:33:00 -0300

    init

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import legacy from "@vitejs/plugin-legacy";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        legacy({
+            targets: ["defaults", "not IE 11"],
+        }),
+    ],
+});
